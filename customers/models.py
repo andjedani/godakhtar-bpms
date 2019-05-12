6dfb02b3b36cb32099bb3d68414c7e4ac9ee3f37
and <and@cafebazaar.ir>
2019-05-13 00:18:26 +0430

    :heavy-check-mark: Take it easy

--- customers/models.py
+++ customers/models.py
@@ -1,162 +1,144 @@
 from django.contrib.postgres.fields import ArrayField
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+
 DEAL_HISTORY_CHOICES = (
-    ('Y', _('Yes')),
-    ('N', _('No')),
-    ('Z', _('Zero'))
+    ('y', _('Yes')),
+    ('n', _('No')),
+    ('z', _('Zero'))
 )
 INQUIRY_HISTORY_CHOICES = (
-    ('N', _('No Inquiry')),
-    ('D', _('Has Deal')),
-    ('R', _('Reject'))
+    ('n', _('No Inquiry')),
+    ('d', _('Has Deal')),
+    ('r', _('Reject'))
 )
 
 DEAL_TYPE_CHOICES = (
-    ('T', _('Gate')),
-    ('G', _('Globe')),
-    ('C', _('Check')),
-    ('O', _('Ball')),
-    ('B', _('Butterfly')),
-    ('R', _('Repair Service')),
-    ('M', _('Maintenance Service')),
-    ('S', _('Spare Parts')),
-    ('H', _('Other'))
+    ('t', _('Gate')),
+    ('g', _('Globe')),
+    ('c', _('Check')),
+    ('o', _('Ball')),
+    ('b', _('Butterfly')),
+    ('r', _('Repair Service')),
+    ('m', _('Maintenance Service')),
+    ('s', _('Spare Parts')),
+    ('h', _('Other'))
 )
 
 ACQUAINTED_CHOICES = (
-    ('E', _('Expo')),
-    ('W', _('Website')),
-    ('R', _('Recommended By Customers')),
-    ('V', _('Vendor List')),
-    ('H', _('Other'))
+    ('e', _('Expo')),
+    ('w', _('Website')),
+    ('r', _('Recommended By Customers')),
+    ('v', _('Vendor List')),
+    ('h', _('Other'))
 )
 
 CUSTOMER_PRIORITY_CHOICES = (
-    ('H', _('High')),
-    ('M', _('Medium')),
-    ('L', _('Low'))
+    ('h', _('High')),
+    ('m', _('Medium')),
+    ('l', _('Low'))
 )
 
 CUSTOMER_SECTION_CHOICES = (
-    ('O', _('Oil')),
-    ('G', _('Gas')),
-    ('P', _('Pertrochemical')),
-    ('R', _('Refinery')),
-    ('E', _('Plant')),
-    ('C', _('Construction')),
-    ('S', _('Steel')),
-    ('H', _('Other'))
+    ('o', _('Oil')),
+    ('g', _('Gas')),
+    ('p', _('Pertrochemical')),
+    ('r', _('Refinery')),
+    ('e', _('Plant')),
+    ('c', _('Construction')),
+    ('s', _('Steel')),
+    ('h', _('Other'))
 )
 
 OWNERSHIP_CHOICES = (
-    ('P', _('Private')),
-    ('G', _('Government')),
-    ('S', _('Semi-Private')),
-    ('H', _('Other'))
+    ('p', _('Private')),
+    ('g', _('Government')),
+    ('s', _('Semi-Private')),
+    ('h', _('Other'))
 )
 
 ACTIVITY_CHOICES = (
-    ('O', _('Operator')),
-    ('C', _('Contractor')),
-    ('S', _('Consultant')),
-    ('H', _('Other'))
+    ('o', _('Operator')),
+    ('c', _('Contractor')),
+    ('s', _('Consultant')),
+    ('h', _('Other'))
 )
 
 CUSTOMER_CLASS_CHOICES = (
-    ('G', _('GC')),
-    ('E', _('EPC')),
-    ('F', _('Finance')),
-    ('O', _('Operator')),
-    ('S', _('Store')),
-    ('H', _('Other'))
+    ('g', _('GC')),
+    ('e', _('EPC')),
+    ('f', _('Finance')),
+    ('o', _('Operator')),
+    ('s', _('Store')),
+    ('h', _('Other'))
 
 )
 
 PERSONNEL_LOCATION_CHOICES = (
-    ('O', _('Office')),
-    ('C', _('Central Office')),
-    ('S', _('Site')),
-    ('H', _('Other'))
+    ('o', _('Office')),
+    ('c', _('Central Office')),
+    ('s', _('Site')),
+    ('h', _('Other'))
 )
 PERSONNEL_TYPE_CHOICES = (
-    ('C', _('CEO')),
-    ('F', _('CFO')),
-    ('T', _('CTO')),
-    ('E', _('Engineering Manager')),
-    ('M', _('Maintenance Manager')),
-    ('S', _('Expert')),
-    ('H', _('Other'))
+    ('c', _('CEO')),
+    ('f', _('CFO')),
+    ('t', _('CTO')),
+    ('e', _('Engineering Manager')),
+    ('m', _('Maintenance Manager')),
+    ('s', _('Expert')),
+    ('h', _('Other'))
 )
-
-ACQUAINTED_CHOICES
 
 
 class Customer(models.Model):
-    # customerName
     name = models.CharField(max_length=200, verbose_name=_('Customer Name'))
-    # customerNumber
     customer_no = models.CharField(max_length=30)
-    # customerPriority
     priority = models.CharField(max_length=1, choices=CUSTOMER_PRIORITY_CHOICES)
-    # financialCode
     financial_code = models.CharField(verbose_name=_('Economical Number'), max_length=20)
-    # nationalId
-    national_id = models.CharField(max_length=20)
-    #  companyCode
-    registration_no = models.CharField(max_length=20)
-    # officeAddress
-    office_address = models.CharField(max_length=255)
-
-    site_address = models.CharField(max_length=255)
-    # postalCode
-    postal_code = models.CharField(max_length=20)
-    # officeFax
-    phone = models.CharField(max_length=20)
-    # officePhone
-    fax = models.CharField(max_length=20)
-    # website
-    website = models.CharField(max_length=127)
-    # companyEmail
-    email = models.CharField(max_length=127)
-    # ceo
-    ceo = models.CharField(max_length=127)
-    # ceoOfficePhone
-    ceo_office = models.CharField(max_length=20)
-    # ceoCellPhone
-    ceo_mobile = models.CharField(max_length=20)
-    # ceoEmail
-    ceo_email = models.CharField(max_length=127)
-    cfo = models.CharField(max_length=127)
-    cfo_office = models.CharField(max_length=20)
-    cfo_email = models.CharField(max_length=127)
-    logistic = models.CharField(max_length=127)
-    logistic_office = models.CharField(max_length=20)
-    logistic_email = models.CharField(max_length=127)
-    engineering = models.CharField(max_length=127)
-    engineering_office = models.CharField(max_length=20)
-    engineering_email = models.CharField(max_length=120)
-    expert = models.CharField(max_length=120)
-    expert_office = models.CharField(max_length=20)
-    expert_email = models.CharField(max_length=120)
-    maintenance_name = models.CharField(max_length=120)
-    maintenance_office = models.CharField(max_length=20)
-    maintenance_email = models.CharField(max_length=120)
+    national_id = models.CharField(max_length=20, blank=True, null=True)
+    registration_no = models.CharField(max_length=20, blank=True, null=True)
+    office_address = models.CharField(max_length=255, blank=True, null=True)
+    site_address = models.CharField(max_length=255, blank=True, null=True)
+    postal_code = models.CharField(max_length=20, blank=True, null=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    fax = models.CharField(max_length=20, blank=True, null=True)
+    website = models.CharField(max_length=127, blank=True, null=True)
+    email = models.CharField(max_length=127, blank=True, null=True)
+    ceo = models.CharField(max_length=127, blank=True, null=True)
+    ceo_office = models.CharField(max_length=20, blank=True, null=True)
+    ceo_mobile = models.CharField(max_length=20, blank=True, null=True)
+    ceo_email = models.CharField(max_length=127, blank=True, null=True)
+    cfo = models.CharField(max_length=127, blank=True, null=True)
+    cfo_office = models.CharField(max_length=20, blank=True, null=True)
+    cfo_email = models.CharField(max_length=127, blank=True, null=True)
+    logistic = models.CharField(max_length=127, blank=True, null=True)
+    logistic_office = models.CharField(max_length=20, blank=True, null=True)
+    logistic_email = models.CharField(max_length=127, blank=True, null=True)
+    engineering = models.CharField(max_length=127, blank=True, null=True)
+    engineering_office = models.CharField(max_length=20, blank=True, null=True)
+    engineering_email = models.CharField(max_length=120, blank=True, null=True)
+    expert = models.CharField(max_length=120, blank=True, null=True)
+    expert_office = models.CharField(max_length=20, blank=True, null=True)
+    expert_email = models.CharField(max_length=120, blank=True, null=True)
+    maintenance_name = models.CharField(max_length=120, blank=True, null=True)
+    maintenance_office = models.CharField(max_length=20, blank=True, null=True)
+    maintenance_email = models.CharField(max_length=120, blank=True, null=True)
     key_persons = ArrayField(models.CharField(max_length=100, blank=True), blank=True, null=True)
     section = models.CharField(max_length=1, choices=CUSTOMER_SECTION_CHOICES)
     ownership = models.CharField(max_length=1, choices=OWNERSHIP_CHOICES)
-    owner = models.CharField(max_length=100)
+    owner = models.CharField(max_length=100, blank=True, null=True)
     activity = models.CharField(max_length=1, choices=ACTIVITY_CHOICES)
     classification = models.CharField(max_length=1, choices=CUSTOMER_CLASS_CHOICES)
     acquainted = models.CharField(max_length=1, choices=ACQUAINTED_CHOICES)
     deal_type = models.CharField(max_length=1, choices=DEAL_TYPE_CHOICES)
-    deal_worth = models.CharField(max_length=100)
+    deal_worth = models.CharField(max_length=127, blank=True, null=True)
     deal_history = models.CharField(max_length=1, choices=DEAL_HISTORY_CHOICES)
     deal_comments = models.TextField()
     inquiry_history = models.CharField(max_length=1, choices=INQUIRY_HISTORY_CHOICES)
-    last_godakhtar_visit = models.DateField()
-    last_customer_visit = models.DateField()
-    mechanism = models.CharField(max_length=100)
+    last_godakhtar_visit = models.CharField(max_length=127, blank=True, null=True)
+    last_customer_visit = models.CharField(max_length=127, blank=True, null=True)
+    mechanism = models.CharField(max_length=100, blank=True, null=True)
     comments = models.TextField()
 
     def __str__(self):
--- customers/views.py
+++ customers/views.py
@@ -1,3 +1,5 @@
+import json
+import os
 from customers import models,serializers
 from rest_framework import viewsets
 
@@ -6,4 +8,22 @@ class CustomerViewSet(viewsets.ModelViewSet):
     queryset = models.Customer.objects
     serializer_class = serializers.CustomerSerializer
 
+    def initial(self, request, *args, **kwargs):
+        log_data = {
+            'user': request.user.pk,
+
+            'remote_address': request.META['REMOTE_ADDR'],
+
+            'request_method': request.method,
+            'request_path': request.get_full_path(),
+            'request_body': request.data ,
+            'request_query_params': request.query_params ,
+            'request_auth': request.auth,
+        }
+        if not os.path.exists('log'):
+            os.makedirs('log')
+
+        with open('log/logging.json', 'w') as f:
+            json.dump(log_data, f, sort_keys=True, indent=4)
+        viewsets.ModelViewSet.initial(self, request, *args, **kwargs)
 
