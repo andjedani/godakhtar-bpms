e130ce110878a942d7aadf2aee2e794ff5e507b1
and <and@cafebazaar.ir>
2019-05-20 02:04:41 +0430

    :sparkles: CORS

--- godakhtar/settings.py
+++ godakhtar/settings.py
@@ -159,9 +159,9 @@ LOGGING = {
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = (
-    r"^https://\w+\.example\.com$",
     '0.0.0.0:8000',
 )
 CORS_ORIGIN_REGEX_WHITELIST = (
+    r"^https://\w+\.example\.com$",
     '0.0.0.0:8000',
 )
