828c66b6a640e8b5b44cb71734633f0a2cb66448
and <and@cafebazaar.ir>
2019-06-02 15:52:04 +0430

    :bank: :pencil2: Change cutomer model as required by customer

--- customers/admin.py
+++ customers/admin.py
@@ -1,14 +1,22 @@
 from django.contrib import admin
-from customers.models import Customer, KeyPerson
+from customers.models import Customer, KeyPerson, Branch, Personnel
 
 
 class KeyPersonAdmin(admin.TabularInline):
     model = KeyPerson
 
 
+class BranchAdmin(admin.TabularInline):
+    model = Branch
+
+
+class PersonnelAdmin(admin.TabularInline):
+    model = Personnel
+
+
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    inlines = [KeyPersonAdmin, ]
+    inlines = [BranchAdmin, PersonnelAdmin, KeyPersonAdmin, ]
     list_display = ("name", "english_name", "customer_no", "priority",)
     list_filter = ("priority", "classification", "activity", "ownership")
     search_fields = ("name", "customer_no", "ceo")
@@ -24,14 +32,7 @@ class CustomerAdmin(admin.ModelAdmin):
               "mechanism",
               "deal_comments",
               "inquiry_history",
-              ("office_address", "site_address", "postal_code"),
-              ("phone", "fax"), ("website", "email"),
-              "ceo", ("ceo_office", "ceo_mobile", "ceo_email"),
-              ("cfo", "cfo_office", "cfo_email"),
-              ("logistic", "logistic_office", "logistic_email"),
-              ("engineering", "engineering_office", "engineering_email"),
-              ("expert", "expert_office", "expert_email"),
-              ("maintenance_name", "maintenance_office", "maintenance_email"),
+              ("website", "email"),
               ("acquainted_expo", "acquainted_website", "acquainted_vendor", "acquainted_other",
                "acquainted_recommended"),
               ("last_godakhtar_visit", "last_customer_visit"),
--- customers/models.py
+++ customers/models.py
@@ -72,22 +72,6 @@ CUSTOMER_CLASS_CHOICES = (
 
 )
 
-PERSONNEL_LOCATION_CHOICES = (
-    ('o', _('Office')),
-    ('c', _('Central Office')),
-    ('s', _('Site')),
-    ('h', _('Other'))
-)
-PERSONNEL_TYPE_CHOICES = (
-    ('c', _('CEO')),
-    ('f', _('CFO')),
-    ('t', _('CTO')),
-    ('e', _('Engineering Manager')),
-    ('m', _('Maintenance Manager')),
-    ('s', _('Expert')),
-    ('h', _('Other'))
-)
-
 CUSTOMER_SIZE_CHOICES = (
     ('s', _("Small")),
     ('m', _("Medium")),
@@ -98,39 +82,18 @@ CUSTOMER_SIZE_CHOICES = (
 class Customer(models.Model):
     name = models.CharField(max_length=200, verbose_name=_('Customer Name'), unique=True)
     english_name = models.CharField(max_length=200, verbose_name=_('English Name'), unique=True)
-    customer_no = models.CharField(max_length=30, null=True, blank=True, unique=True)
-    priority = models.CharField(max_length=1, choices=CUSTOMER_PRIORITY_CHOICES, blank=True, null=True)
-    financial_code = models.CharField(verbose_name=_('Economical Number'), max_length=20, null=True, blank=True, unique=True)
-    national_id = models.CharField(max_length=20, blank=True, null=True, unique=True)
-    registration_no = models.CharField(max_length=20, blank=True, null=True)
-    office_address = models.CharField(max_length=255, blank=True, null=True)
-    site_address = models.CharField(max_length=255, blank=True, null=True)
-    postal_code = models.CharField(max_length=20, blank=True, null=True, unique=True)
-    phone = models.CharField(max_length=20, blank=True, null=True)
-    fax = models.CharField(max_length=20, blank=True, null=True)
-    website = models.CharField(max_length=127, blank=True, null=True)
-    email = models.CharField(max_length=127, blank=True, null=True)
-    ceo = models.CharField(max_length=127, blank=True, null=True)
-    ceo_office = models.CharField(max_length=20, blank=True, null=True)
-    ceo_mobile = models.CharField(max_length=20, blank=True, null=True)
-    ceo_email = models.CharField(max_length=127, blank=True, null=True)
-    cfo = models.CharField(max_length=127, blank=True, null=True)
-    cfo_office = models.CharField(max_length=20, blank=True, null=True)
-    cfo_email = models.CharField(max_length=127, blank=True, null=True)
-    logistic = models.CharField(max_length=127, blank=True, null=True)
-    logistic_office = models.CharField(max_length=20, blank=True, null=True)
-    logistic_email = models.CharField(max_length=127, blank=True, null=True)
-    engineering = models.CharField(max_length=127, blank=True, null=True)
-    engineering_office = models.CharField(max_length=20, blank=True, null=True)
-    engineering_email = models.CharField(max_length=120, blank=True, null=True)
-    expert = models.CharField(max_length=120, blank=True, null=True)
-    expert_office = models.CharField(max_length=20, blank=True, null=True)
-    expert_email = models.CharField(max_length=120, blank=True, null=True)
-    maintenance_name = models.CharField(max_length=120, blank=True, null=True)
-    maintenance_office = models.CharField(max_length=20, blank=True, null=True)
-    maintenance_email = models.CharField(max_length=120, blank=True, null=True)
-    customer_size = models.CharField(max_length=1, choices=CUSTOMER_SIZE_CHOICES, null=True, blank=True)
-    oil_section = models.BooleanField(help_text="Section", default=False, verbose_name=_('Oil'))
+    customer_no = models.CharField(verbose_name=_('Customer Number'), max_length=30, null=True, blank=True, unique=True)
+    priority = models.CharField(verbose_name=_('Priority'), max_length=1, choices=CUSTOMER_PRIORITY_CHOICES, blank=True,
+                                null=True)
+    financial_code = models.CharField(verbose_name=_('Economical Number'), max_length=20, null=True, blank=True,
+                                      unique=True)
+    national_id = models.CharField(verbose_name=_('National ID'), max_length=20, blank=True, null=True, unique=True)
+    registration_no = models.CharField(verbose_name=_('Registration Number'), max_length=20, blank=True, null=True)
+    postal_code = models.CharField(verbose_name=_('Postal Code'), max_length=20, blank=True, null=True, unique=True)
+    website = models.CharField(verbose_name=_('Website address'), max_length=127, blank=True, null=True)
+    email = models.CharField(verbose_name=_('email address'), max_length=127, blank=True, null=True)
+    customer_size = models.CharField(max_length=1, choices=CUSTOMER_SIZE_CHOICES, blank=False, null=False)
+    oil_section = models.BooleanField(default=False, verbose_name=_('Oil'))
     gas_section = models.BooleanField(default=False, verbose_name=_('Gas'))
     pertrochemical_section = models.BooleanField(default=False, verbose_name=_('Pertrochemical'))
     refinery_section = models.BooleanField(default=False, verbose_name=_('Refinery'))
@@ -141,7 +104,7 @@ class Customer(models.Model):
     ownership = models.CharField(max_length=1, choices=OWNERSHIP_CHOICES, blank=True, null=True)
     owner = models.CharField(max_length=100, blank=True, null=True)
     activity = models.CharField(max_length=1, choices=ACTIVITY_CHOICES, blank=True, null=True)
-    classification = models.CharField(max_length=1, choices=CUSTOMER_CLASS_CHOICES, blank=True, null=True)
+    classification = models.CharField(max_length=1, choices=CUSTOMER_CLASS_CHOICES, blank=False, null=False)
     acquainted_expo = models.BooleanField(default=False, verbose_name=_('Expo'))
     acquainted_website = models.BooleanField(default=False, verbose_name=_('Website'))
     acquainted_vendor = models.BooleanField(default=False, verbose_name=_('Vendor List'))
@@ -170,12 +133,13 @@ class Customer(models.Model):
              update_fields=None):
         if not self.pk:
             customer_no_draft = self.classification + self.customer_size
-            same_customers = Customer.objects.filter(classification=self.classification, customer_size=self.customer_size).order_by('-customer_no')
+            same_customers = Customer.objects.filter(classification=self.classification,
+                                                     customer_size=self.customer_size).order_by('-customer_no')
             serial = 1
             if same_customers:
-                serial = int(same_customers[0].customer_no[2:])+1
+                serial = int(same_customers[0].customer_no[2:]) + 1
 
-            customer_no_draft = customer_no_draft.upper() + f'{serial:3}'.replace(' ','0')
+            customer_no_draft = customer_no_draft.upper() + f'{serial:3}'.replace(' ', '0')
             self.customer_no = customer_no_draft
             super().save()
 
@@ -183,10 +147,49 @@ class Customer(models.Model):
         return self.name
 
 
+PERSONNEL_TYPE_CHOICES = (
+    ('c', _('CEO')),
+    ('f', _('CFO')),
+    ('t', _('CTO')),
+    ('e', _('Engineering Manager')),
+    ('m', _('Maintenance Manager')),
+    ('s', _('Expert')),
+    ('h', _('Other'))
+)
+
+
+class Personnel(models.Model):
+    name = models.CharField(max_length=127, blank=False)
+    role = models.CharField(max_length=2, choices=PERSONNEL_TYPE_CHOICES, blank=False)
+    office_phone = models.CharField(max_length=20, blank=True, null=True)
+    mobile = models.CharField(max_length=20, blank=True, null=True)
+    email = models.CharField(max_length=127, blank=True, null=True)
+    comments = models.CharField(max_length=255, null=True, blank=True)
+    customer = models.ForeignKey(to='Customer', on_delete=models.CASCADE)
+
+
 class KeyPerson(models.Model):
     name = models.CharField(max_length=127, blank=False)
     office = models.CharField(max_length=20, blank=True, null=True)
     mobile = models.CharField(max_length=20, blank=True, null=True)
     email = models.CharField(max_length=127, blank=True, null=True)
-    comments = models.TextField(null=True)
+    comments = models.CharField(max_length=255, blank=True, null=True)
     customer = models.ForeignKey(to='Customer', on_delete=models.CASCADE)
+
+
+BRANCH_TYPE_CHOICES = (
+    ('c', _("Headquarters")),
+    ('o', _("Office")),
+    ('s', _("Site")),
+    ('h', _("Other"))
+)
+
+
+class Branch(models.Model):
+    name = models.CharField(max_length=127, blank=False)
+    type = models.CharField(max_length=1, choices=BRANCH_TYPE_CHOICES)
+    address = models.CharField(verbose_name=_('Address'), max_length=255, blank=True, null=True)
+    postal_code = models.CharField(verbose_name=_('Postal Code'), max_length=20, blank=True, null=True, unique=True)
+    phone = models.CharField(verbose_name=_('Phone Number'), max_length=20, blank=True, null=True)
+    fax = models.CharField(verbose_name=_('Fax Number'), max_length=20, blank=True, null=True)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
