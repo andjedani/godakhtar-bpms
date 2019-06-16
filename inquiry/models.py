b87e30a675cb62714da37b00c7607584a403c377
and <and@cafebazaar.ir>
2019-06-16 23:02:14 +0430

    :pencil2: Minor changes to Inquiry model: def str

--- inquiry/models.py
+++ inquiry/models.py
@@ -13,6 +13,9 @@ class Inquiry(models.Model):
     packaging = models.CharField(max_length=5, blank=True, null=True, verbose_name=_('Packaging'))
     delivery_address = models.CharField(max_length=255, blank=True, null=True, verbose_name=_('Delivery Address'))
 
+    def __str__(self):
+        return self.customer.name
+
 
 class InquiryProducts(models.Model):
     inquiry = models.ForeignKey(Inquiry, on_delete=models.CASCADE, related_name='inquiry_products')
--- inquiry/serializers.py
+++ inquiry/serializers.py
@@ -9,41 +9,12 @@ class InquiryProductsSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-# class PersonnelSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Personnel
-#         fields = '__all__'
-#
-#
-# class KeyPersonSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = KeyPerson
-#         fields = '__all__'
-#
-#
 class InquirySerializer(serializers.ModelSerializer):
     inquiry_products = InquiryProductsSerializer(many=True, read_only=True)
-
-    # personnel = PersonnelSerializer(many=True)
-    # key_persons = KeyPersonSerializer(many=True)
-    # customer = CustomerSerializer
 
     class Meta:
         model = Inquiry
         fields = "__all__"
-        # "financial_code", "national_id", "registration_no",
-        # "classification", "activity", "customer_size", "priority",
-        # "oil_section", "gas_section", "pertrochemical_section", "refinery_section", "plant_section",
-        # "ownership", "owner",
-        # "deal_type_gate", "deal_type_globe", "deal_type_check", "deal_type_ball", "deal_type_butterfly",
-        # "deal_type_repair_service", "deal_type_spare_parts", "deal_type_other", "deal_type_maintenance",
-        # "deal_worth", "deal_history",
-        # "mechanism",
-        # "deal_comments",
-        # "inquiry_history",
-        # "website", "email",
-        # "acquainted_expo", "acquainted_website", "acquainted_vendor", "acquainted_other",
-        # "acquainted_recommended",
 #                   "last_godakhtar_visit", "last_customer_visit",
 #                   "comments", "verified", "branchs", "personnel", "key_persons")
 #
